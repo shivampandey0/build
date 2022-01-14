0459caf2df4bf44de935d18d03204204b68fe675
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-01-14 17:47:35 +0530

    date validation

--- solutions/functions/12.js
+++ solutions/functions/12.js
@@ -2,15 +2,24 @@
 //     Example:
 // Input: minDate('02/05/2021', '24/01/2021') ––> Output: 24 / 01 / 2021
 
-const minDate = (date1,date2) => {
+const minDate = (date1, date2) => {
+    const formattedDate1 = date1.split('/')
+    const formattedDate2 = date2.split('/')
 
-    
-    
-    if (Date.parse(date1) < Date.parse(date2)) {
-        return `${date1} comes before ${date2}`
-    }else{
-        return `${date2} comes before ${date1}`
+    if (isDateValid(...formattedDate1) && isDateValid(...formattedDate2)) {
+        if (Date.parse(date1) < Date.parse(date2)) {
+            return `${date1} comes before ${date2}`
+        } else {
+            return `${date2} comes before ${date1}`
+        }
+    } else {
+        return `One of the date is invalid`
     }
 }
 
-console.log(minDate('02/05/2022', '02/31/2021'));
+const isDateValid = (day, month, year) => {
+    let date = new Date(year, month, day);
+    return date.getFullYear() == year && date.getMonth() == month && date.getDate() == day;
+}
+
+console.log(minDate('02/05/2021', '24/01/2021'));
