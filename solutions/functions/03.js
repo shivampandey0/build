214aba9690392b19dce29db93c18752175b2cb95
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-01-14 13:16:19 +0530

    split function for string

--- solutions/functions/03.js
+++ solutions/functions/03.js
@@ -2,6 +2,26 @@
 //     Example:
 // Input: noOfWords(We are neoGrammers) ––> Output: 3
 
-let noOfWords = sentence => sentence.trim().split(' ').length
+// let noOfWords = sentence => sentence.trim().split(' ').length
+let noOfWords = sentence => {
+    sentence = sentence.trim()
+    let word =''
+    let splittedArray=[]
+
+    console.log(sentence);
+    
+    for(let i=0; i<=sentence.length;i++){
+        console.log(word);
+        if (sentence[i]=== ' ' || i === sentence.length) {
+            splittedArray.push(word)
+            word =''
+        }else{
+            word = word + sentence[i]
+        }
+    }
+
+    console.log(splittedArray);
+    return splittedArray.length
+}
 
 console.log(noOfWords('We are neoGrammers  '));
