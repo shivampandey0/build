3334bf552ae68fed4133dd6a9f632594dbfc21ba
Shivam Pandey <pandeyshivam1312@gmail.com>
2022-01-18 16:37:26 +0530

    last exercise of strings

---
+++ solutions/strings/21.js
@@ -0,0 +1,26 @@
+// Given a string "how was your day?" and a word "how", write a program that removes the occurrence of the specified word from given sentence. (input: string⇒"programming camp are amazing", word⇒ "programming"; output: " camp are amazing")
+
+const removeWord = (str, word) => {
+    let tempWord = '';
+    let newStr = '';
+
+    for (const i in str) {
+        if(str[i].toLowerCase() != str[i].toUpperCase()){
+            tempWord+=str[i];
+        }else if(tempWord == word){
+            tempWord ='';
+            newStr+=str[i];
+        }else{
+            newStr += tempWord + str[i];            
+            tempWord = '';
+        }
+
+        if (i == str.length-1) newStr += tempWord;
+        
+    }
+
+    return newStr;
+}
+
+console.log(removeWord("how are you, how was your day?", 'how'));
+console.log(removeWord("programming camp are amazing", 'programming'));
